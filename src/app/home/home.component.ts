b6b821861bd90b8c7a21309a2d557bb9534a5d64
Tuba <brunogabrielsd@hotmail.com>
2023-11-20 00:36:01 -0300

    feat(home): read stream of messages from firestore

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MessageListComponent } from "./ui/message-list.component";
+import { MessageService } from '../shared/data-access/message.service';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  template: `<p>Hello World!</p>`,
+  template: `
+    <div class="container">
+      <app-message-list [messages]="messageService.messages()" />
+    </div>
+  `,
+  imports: [MessageListComponent]
 })
 export default class HomeComponent {
-
+  messageService = inject(MessageService);
 }
