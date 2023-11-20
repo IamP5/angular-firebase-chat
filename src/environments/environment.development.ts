f634ceec24ef1241b0467294ba19b8580d6bd0d6
Tuba <brunogabrielsd@hotmail.com>
2023-11-20 00:13:04 -0300

    feat: firebase setup

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCZvcvfUCOoXNZlszdom3-ljt_qHFNq4H4",
+    authDomain: "as-ng-chat-app.firebaseapp.com",
+    projectId: "demo-project",
+    storageBucket: "as-ng-chat-app.appspot.com",
+    messagingSenderId: "896863530888",
+    appId: "1:896863530888:web:b3845b1fd26326e389d43d"
+  },
+  production: false,
+  useEmulators: true,
+};
